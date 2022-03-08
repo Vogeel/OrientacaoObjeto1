81fd270bf3700c4adca215ac772f7c5cd32175d0
Vogeel <vitorvogel12@gmail.com>
2022-03-08 14:44:27 -0300

    Add project files.

---
+++ OrientacaoObjeto.ConsoleApp/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto.ConsoleApp
+{
+    internal class Caixa
+    {
+        public int altura;
+        public int largura;
+        public int comprimento;
+
+        public double ValorFinal()
+        {
+            double Volume = altura * largura * comprimento;
+            return Volume;
+        }
+    }
+}
---
+++ OrientacaoObjeto.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OrientacaoObjeto.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Caixa caixa = new Caixa();
+
+            caixa.altura = 10;
+            caixa.largura = 10;
+            caixa.comprimento = 10;
+
+            Console.WriteLine( "O volume da caixa é: " + caixa.ValorFinal());
+            Console.ReadKey();
+
+        }
+    }
+}
